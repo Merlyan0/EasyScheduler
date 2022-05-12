84964eac1113c4029a4b40de6a4054607cb654b6
Merlyan0 <48512605+Merlyan0@users.noreply.github.com>
2022-05-12 21:13:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
+# VK API
 TOKEN = 'ВАШ_ТОКЕН'
 GROUP_ID = 'ID_ВАШЕЙ_ГРУППЫ_ВК'
+
+# MySQL DataBase
+DB_HOST = 'HOST_ДЛЯ_ПОДКЛЮЧЕНИЯ_К_MYSQL'
+DB_USER = 'USER_ДЛЯ_ПОДКЛЮЧЕНИЯ_К_MYSQL'
+DB_PASSWORD = 'PASSWORD_ДЛЯ_ПОДКЛЮЧЕНИЯ_К_MYSQL'
+DB_DATABASE = 'ИМЯ_БД_ДЛЯ_ПОДКЛЮЧЕНИЯ_К_MYSQL'
