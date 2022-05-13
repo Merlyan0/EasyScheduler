b9e55cec0a25fa3cba2f0b4f03b02f294283dcaa
Merlyan0 <48512605+Merlyan0@users.noreply.github.com>
2022-05-13 23:23:30 +0300

    Глобальные изменения в работе с БД
    1. Подключение к БД реализовано в виде класса
    2. Будет использоваться кодировка unicode для всего
    3. Добавлена поддержка смайликов

--- data/scripts/db.py
+++ data/scripts/db.py
@@ -1,60 +1,71 @@
+from datetime import datetime
+
 import pymysql
 from pymysql.cursors import DictCursor
 
 from config import *
 
 
-def create_connection() -> tuple:
-    """
-    Подключение к БД. Возвращает соединение к базе данных и курсор.
-    """
-    conn = pymysql.connect(
-        host=DB_HOST,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        db=DB_DATABASE,
-        charset='utf8mb4',
-        cursorclass=DictCursor
-    )
-    cur = conn.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS reminders(
-                     id integer AUTO_INCREMENT PRIMARY KEY,
-                     title text,
-                     author int,                     
-                     attachments text,
-                     check_date datetime,
-                     finished bool,
-                    created_date datetime);""")
-    conn.commit()
+class DataBase:
 
-    print('Подключение к базе данных успешно!')
+    def __init__(self):
+        """
+        Подключение к БД.
+        """
+        self.conn = pymysql.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            db=DB_DATABASE,
+            charset='utf8mb4',
+            cursorclass=DictCursor,
+            autocommit=True,
+            use_unicode=True
+        )
 
-    return conn, cur
+        self.cur = self.conn.cursor()
 
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS reminders(
+                         id integer AUTO_INCREMENT PRIMARY KEY,
+                         title text,
+                         author int,                     
+                         attachments text,
+                         check_date datetime,
+                         finished bool,
+                        created_date datetime);""")
+        self.conn.commit()
 
-def add_to_db(conn, cur, title=None, author=None, attachments=None,
-              check_date=None, finished=None, created_date=None) -> None:
-    """
-    Добавление нового значения в базу данных.
-    """
-    cur.execute(f"""INSERT INTO reminders(
-                         title,
-                         author,
-                         attachments,
-                         check_date,
-                         finished,
-                         created_date)
-           VALUES (%s, %s, %s, %s, %s, %s);""", (title, author, attachments,
-                                                 check_date, finished, created_date))
-    conn.commit()
+        self.cur.execute('ALTER TABLE reminders CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin')
+        self.conn.commit()
 
+        print('EasyScheduler >', f'Подключение к базе данных {DB_DATABASE} успешно.')
 
-def set_date(conn, cur, author, date) -> None:
-    """
-    Задание даты для последнего напоминания пользователя.
-    """
-    cur.execute(f"SELECT * FROM reminders WHERE author={author} ORDER BY id DESC LIMIT 1")
-    reminder_id = cur.fetchone()['id']
-    
-    cur.execute(f'UPDATE reminders SET check_date = %s WHERE id = %s', (date, reminder_id))
-    conn.commit()
+    def add_to_db(self, title=None, author=None, attachments=None,
+                  check_date=None, finished=None, created_date=datetime.now()) -> None:
+        """
+        Добавление нового значения в базу данных.
+        """
+        self.cur.execute(f"""INSERT INTO reminders(
+                             title,
+                             author,
+                             attachments,
+                             check_date,
+                             finished,
+                             created_date)
+               VALUES (%s, %s, %s, %s, %s, %s);""", (title.encode("unicode-escape"), author, attachments,
+                                                     check_date, finished, created_date))
+        self.conn.commit()
+
+    def get_actual_reminders(self, date):
+        self.cur.execute(f"SELECT * FROM reminders WHERE finished=0 and check_date=%s", (date, ))
+        return self.cur.fetchall()
+
+    def set_date(self, author, date) -> None:
+        """
+        Задание даты для последнего напоминания пользователя.
+        """
+        self.cur.execute(f"SELECT * FROM reminders WHERE author={author} ORDER BY id DESC LIMIT 1")
+        reminder_id = self.cur.fetchone()['id']
+
+        self.cur.execute(f'UPDATE reminders SET check_date = %s WHERE id = %s', (date, reminder_id))
+        self.conn.commit()
