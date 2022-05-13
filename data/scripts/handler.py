93d19ffa9dd94ed1102bd8b97a4e097e0a66a393
Merlyan0 <48512605+Merlyan0@users.noreply.github.com>
2022-05-13 15:41:30 +0300

    Изменение структуры кода

--- data/scripts/handler.py
+++ data/scripts/handler.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from vk_api.utils import get_random_id
 
 from DateParser.parser import analyze_string
+from data.scripts import messages
 from data.scripts.db import add_to_db, set_date
 from data.scripts.functions import check_date
 from data.scripts.keyboards import *
@@ -70,46 +71,32 @@ class BotHandler:
                                 delete_for_all=True)
 
     def create_manually(self, event):
-        # id предыдущего сообщение
-        conv_mess_id = event.object.message["conversation_message_id"] - 1
-        # предыдущее сообщение
-        prev_mess = self.vk.messages.getByConversationMessageId(peer_id=event.object.message["peer_id"],
-                                                                conversation_message_ids=conv_mess_id)
+        add_to_db(self.db_conn, self.db_cur, title=event.object.message["text"],
+                  author=event.object.message["from_id"],
+                  finished=False, created_date=datetime.now())
 
-        print(prev_mess)
+        self.vk.messages.send(peer_id=event.object.message["peer_id"],
+                              message=MESS_CREATE_REMINDER_COMPLETED_1,
+                              random_id=get_random_id())
 
-        if prev_mess['items'][0]['text'] == MESS_CREATE_REMINDER:
-            add_to_db(self.db_conn, self.db_cur, title=event.object.message["text"],
-                      author=event.object.message["from_id"],
-                      finished=False, created_date=datetime.now())
+    def create_manually_step2(self, event):
+        date = event.object.message["text"]
 
-            self.vk.messages.send(peer_id=event.object.message["peer_id"],
-                                  message=MESS_CREATE_REMINDER_COMPLETED_1,
-                                  random_id=get_random_id())
+        date = check_date(date)
 
-            return True
-
-        elif prev_mess['items'][0]['text'] == MESS_CREATE_REMINDER_COMPLETED_1:
-            date = event.object.message["text"]
-
-            date = check_date(date)
-
-            set_date(self.db_conn, self.db_cur, event.object.message['from_id'], date.strftime('%Y-%m-%d %H:%M:%S'))
-
-            return True
-
-        return False
+        set_date(self.db_conn, self.db_cur, event.object.message['from_id'], date.strftime('%Y-%m-%d %H:%M:%S'))
 
     def reminder_analyzer(self, event):
         a = analyze_string(event.object.message["text"].lower())
 
-        # DataParser вернул сообщение об ошибке
+        # DateParser вернул сообщение об ошибке
         if a[0] == 'Ошибка':
             self.vk.messages.send(peer_id=event.object.message["peer_id"],
                                   message=f"&#10060; {a[0]} \n\n"
                                           f"{a[1]}",
                                   random_id=get_random_id())
 
+        # DateParser не нашел даты и времени в сообщении
         elif a[2]['date'] is False and a[2]['time'] is False:
             title = ' '.join(a[1]).capitalize()
 
@@ -126,6 +113,7 @@ class BotHandler:
                                   random_id=get_random_id(),
                                   keyboard=keyboard.get_keyboard())
 
+        # DateParser не нашел времени в сообщении
         elif a[2]['time'] is False:
             title = ' '.join(a[1]).capitalize()
             date = a[0].strftime('%d.%m.%Y')
@@ -143,6 +131,7 @@ class BotHandler:
                                   random_id=get_random_id(),
                                   keyboard=keyboard.get_keyboard())
 
+        # DateParser обнаружил всё необходимое
         else:
             title = ' '.join(a[1]).capitalize()
             time = a[0].strftime('%d.%m.%Y в %H:%M')
@@ -159,3 +148,7 @@ class BotHandler:
                                   message=MESS_REMINDER_RECEIVED.substitute(title=title, time=time),
                                   random_id=get_random_id(),
                                   keyboard=keyboard.get_keyboard())
+    
+    @classmethod
+    def get_message(cls, message):
+        return getattr(messages, message)
