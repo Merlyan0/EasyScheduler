4be3234755949a4cbf540db0b8120bbb17a9a2af
Merlyan0 <48512605+Merlyan0@users.noreply.github.com>
2022-05-17 22:27:17 +0300

    Добавлены новые клавиатуры

--- data/scripts/keyboards.py
+++ data/scripts/keyboards.py
@@ -1,19 +1,28 @@
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 # главное меню
-KB_MAIN_MENU = VkKeyboard(one_time=True)
+KB_MAIN_MENU = VkKeyboard(one_time=False)
 
 KB_MAIN_MENU.add_button('&#128195; Список напоминаний')
 
 KB_MAIN_MENU.add_line()
-KB_MAIN_MENU.add_button('&#128221; Создать напоминание', color=VkKeyboardColor.SECONDARY)
-KB_MAIN_MENU.add_button('&#9989; Завершить напоминание', color=VkKeyboardColor.SECONDARY)
+KB_MAIN_MENU.add_button('&#128221; Ручной режим', color=VkKeyboardColor.SECONDARY)
 
 KB_MAIN_MENU.add_line()
 KB_MAIN_MENU.add_button('&#9881; Настройки', color=VkKeyboardColor.PRIMARY)
 
 
+# меню ручного ввода
+KB_MANUAL_MODE = VkKeyboard(one_time=False)
+
+KB_MANUAL_MODE.add_button('&#128221; Создать напоминание', color=VkKeyboardColor.SECONDARY)
+KB_MANUAL_MODE.add_button('&#9989; Завершить напоминание', color=VkKeyboardColor.SECONDARY)
+
+KB_MANUAL_MODE.add_line()
+KB_MANUAL_MODE.add_button('&#128281; Главное меню')
+
+
 # меню возврата в главное меню
-KB_BACK = VkKeyboard(one_time=True)
+KB_BACK = VkKeyboard(one_time=False)
 
 KB_BACK.add_button('&#128281; Главное меню')
